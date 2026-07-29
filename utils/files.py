50f4d333d60fd3780a1dacdac1cb4273175ef697
JamestheCog <jcog196013@gmail.com>
2026-07-29 09:52:52 +0800

    chore: added typings

--- utils/files.py
+++ utils/files.py
@@ -1,5 +1,7 @@
-def to_mb(raw_size: float) -> float:
+from typing import Union
+
+def to_mb(raw_size: Union[float, int]) -> float:
     '''
     Converts raw byte values to Mbs
     '''
-    return raw_size / 1024**2 if raw_size else 0
+    return raw_size / 1024**2 if raw_size else 0.0
